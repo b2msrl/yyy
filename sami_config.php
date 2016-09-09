11074acbc2608a0c7559125869ed617160f71a4a
alevento <a@a.it>
2016-09-09 11:12:44 +0200

    Add Sami Config File

---
+++ sami_config.php
@@ -0,0 +1,28 @@
+<?php
+
+use Sami\Sami;
+use Sami\RemoteRepository\GitHubRemoteRepository;
+use Sami\Version\GitVersionCollection;
+use Symfony\Component\Finder\Finder;
+
+$iterator = Finder::create()
+    ->files()
+    ->exclude('Vendor')
+    ->exclude('tests')
+    ->in($dir = __DIR__.'/src')
+;
+
+$versions = GitVersionCollection::create($dir)
+    ->addFromTags('*')
+    ->add('master','master')
+;
+
+return new Sami($iterator,array(
+    'theme'                => 'default',
+    'title'                => 'Workbench API',
+    'versions'             => $versions,
+    'build_dir'            => 'Y:/build/%version%',
+    'cache_dir'            => 'Y:/cache/%version%',
+    'default_opened_level' => 2,
+));
+
